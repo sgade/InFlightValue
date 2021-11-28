217e8bd745f952d8519ffc4440d03aad0c3f5bf8
Sören Gade <gade.soeren@gmail.com>
2021-11-29 00:53:43 +0100

    Add documentation.

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # InFlightValue
 
-A description of this package.
+Awaits already in-flight calls and their value.
+
+## Usage
+
+For each call that should only be executed one at a time, create a new instace of `InFlightValueProvider`:
+
+```swift
+let value = InFlightValueProvider {
+    [...]
+    let (data, _) = try await URLSession.default.data(for: request)
+    return data
+}
+
+let result1 = try await value.get()
+// from another queue...
+let result2 = try await value.get()
+```
+
+This ensures that subsequent calls to the value while the request is in-flight will return with the same result as the
+original call. Once the value has been returned to all waiting threads, the provider will do its work once again.
+
+## Installation
+
+Using the Swift Package Manager, add this line to your `Package.swift`:
+
+```swift
+    .package(url: "https://github.com/sgade/InFlightValue", from: "1.0.0"),
+```
--- Sources/InFlightValue/InFlightValueProvider.swift
+++ Sources/InFlightValue/InFlightValueProvider.swift
@@ -1,22 +1,33 @@
 //
-//  InFlightValue.swift
+//  InFlightValueProvider.swift
 //  InFlightValue
 //
 //  Created by Sören Gade on 29.11.21.
 //  Copyright © 2021 Sören Gade. All rights reserved.
 //
 
+/// Provides access to in-flight calls to values of a producer.
 public actor InFlightValueProvider<Value> {
 
+    /// Provides a `Value` or throws an `Error`.
     public typealias ValueProducer = () async throws -> Value
 
     private var producer: ValueProducer
+    /// A possible in-flight call. `nil` if not awaiting any calls currently.
     private var activeTask: Task<Value, Error>?
 
     public init(_ producer: @escaping ValueProducer) {
         self.producer = producer
     }
 
+    /**
+     Returns the `Value` of the producer, or its `Error`.
+
+     - If no operation is currently active, this will call the producer to aquire a value to return.
+     - If there currently is an active operation, this will await that operation and returns its result.
+
+     - Returns: The producer's `Value` or `Error`.
+     */
     public func get() async throws -> Value {
         if let activeTask = activeTask {
             return try await activeTask.value
